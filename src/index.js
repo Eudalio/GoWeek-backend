0a2ea206c256737c6fa1baa732bcd60453393c4d
eudalio <tonnydallios@gmail.com>
2018-11-06 09:26:40 -0300

    Conectado com o banco

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const uriDB = 'mongodb://eudalio:eudalio123@ds143071.mlab.com:43071/goweek-backend';
+const app = express();
+
+mongoose.connect(uriDB, {
+    useNewUrlParser: true
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})

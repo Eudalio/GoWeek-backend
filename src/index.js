e2dbe14a896394ed5e89fd967a2a8aec85f9bab5
eudalio <tonnydallios@gmail.com>
2018-11-06 22:43:58 -0300

    Criado Tweet Controller

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ mongoose.connect(uriDB, {
     useNewUrlParser: true
 });
 
+app.use(express.json())
+app.use(require('./routes'));
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
